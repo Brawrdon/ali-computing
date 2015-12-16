f4a91c78595a4a441c86ab7b10c7cce75ac50545
Brandon <BrandonOkeke@gmail.com>
2015-12-16 10:55:36 +0000

    Retrieve data from database

--- login.php
+++ login.php
@@ -19,15 +19,32 @@
 	if(mysqli_num_rows($username_query) > 0)
 	{
 		echo "<p>Username found</p>";
+		
+		$username_record = mysqli_fetch_array($username_query, MYSQLI_NUM);
+		
+		# Data taken from database	
+		echo "<h3>Data taken from database</h3>";
+		$user_id = $username_record[0];
+		$user_fname = $username_record[1];
+		$user_lname = $username_record[2];
+		$user_password = $username_record[4];
+		
+		echo "<p><b>ID:</b> $user_id</p>";
+		echo "<p><b>Name:</b> $user_fname $user_lname</p>";
+		echo "<p><b>Password:</b> $user_password</p>";
 	}
 	else
 	{
 		echo "<p>Username not found</p>";
 	}
 	
-	# Echoes out the data		
+	# Echoes out the data	
+	echo "<h3><b>Data inputed</b></h3>";
 	echo "<p><b>Username:</b> $login_username<br>";
 	echo "<p><b>Password:</b> $login_password";
+	
+	# Free memeory from the result
+	mysqli_free_result($username_query);
 	
 	# Closes the connection
 	mysqli_close($connection);
