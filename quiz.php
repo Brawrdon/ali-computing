c2afa9a979976285487b87007a787ca42cfdc5a9
Brandon <BrandonOkeke@gmail.com>
2015-12-25 15:44:02 +0000

    Added comments

--- quiz.php
+++ quiz.php
@@ -1,17 +1,25 @@
 <?php
+	# Runs the connection script
 	require_once("connect.php");
+	
+	# Outputs the data found in the saved cookies
 	echo "<p> The topic ID from the cookie is: " . $_COOKIE["topic_id"] . "</p>";
 	echo "<p> The user ID from the cookie is: " . $_COOKIE["login"] . "</p>";
 	
+	# Sets the ID to the data saved in the cookies
 	$topic_id = $_COOKIE["topic_id"];
+	
 	$quiz_sql = "SELECT * FROM questions WHERE topic_id = $topic_id ";
 	
 	echo $quiz_sql;
 	
+	# Runs the MySQL query
 	$quiz_query = mysqli_query($connection, $quiz_sql);
 	
+	# Checks the results of the query
 	if(mysqli_num_rows($quiz_query) > 0)
 	{
+		# Takes the record and outputs the required data
 		$question_record = mysqli_fetch_assoc($quiz_query);
 		$question_title = $question_record["question_title"];
 		$answer_1 = $question_record["question_answer1"];
--- topics.php
+++ topics.php
@@ -1,4 +1,5 @@
 <?php
+	# Runs the connection script
 	require_once("connect.php");
 	
 	echo "<h2><center><b>Topics List</b></center></h2>";
@@ -7,11 +8,15 @@
 	
 	echo $topic_sql;
 	
+	# Runs MySQL query
 	$topic_query = mysqli_query($connection, $topic_sql);
+	
+	#Checks the result of the query
 	if(mysqli_num_rows($topic_query) > 0)
 	{
 		echo "<p>There is data here</p>";
 	
+		# Gets each record, outputting them and setting an ID to the buttons
 		while($topic_record = mysqli_fetch_assoc($topic_query))
 		{
 			$topic_id = $topic_record["topic_id"];
