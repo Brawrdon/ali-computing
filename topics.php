1fbe6268879614e3fa5f91729b95521357bd8979
Brandon <BrandonOkeke@gmail.com>
2015-12-25 15:19:10 +0000

    Cleaned up code

--- topics.php
+++ topics.php
@@ -8,11 +8,10 @@
 	echo $topic_sql;
 	
 	$topic_query = mysqli_query($connection, $topic_sql);
-	
 	if(mysqli_num_rows($topic_query) > 0)
 	{
 		echo "<p>There is data here</p>";
-		
+	
 		while($topic_record = mysqli_fetch_assoc($topic_query))
 		{
 			$topic_id = $topic_record["topic_id"];
@@ -21,10 +20,10 @@
 			<input type=\"hidden\" name=\"topic\" value=\"$topic_id\">
 			<input type=\"submit\" value=\"Start Quiz\"></form></p>";	
 		}
-
 	}
 	else
 	{
 		echo "<p>There is no data here</p>";
-	}		
+	}
+
 ?>
