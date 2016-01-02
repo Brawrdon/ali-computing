4ccd5dcc637dd6fa0d78cf5404281c7aa139b5db
Brandon <BrandonOkeke@gmail.com>
2016-01-02 18:32:15 +0000

    Automatically increments question number

--- quiz.php
+++ quiz.php
@@ -2,10 +2,10 @@
 	# Runs the connection script
 	require_once("connect.php");
 	
-	function question(){
+	function question($number){
 	
 		global $topic_id, $connection;
-		$quiz_sql = "SELECT * FROM questions WHERE topic_id = $topic_id";
+		$quiz_sql = "SELECT * FROM questions WHERE topic_id = $topic_id AND question_number = $number";
 	
 		echo $quiz_sql;
 	
@@ -18,6 +18,9 @@
 			# Takes the record and outputs the required data
 			$question_record = mysqli_fetch_assoc($quiz_query);
 			$question_title = $question_record["question_title"];
+			# Creates a question number cookie to be edited in the sumbit file
+			$question_number = $question_record["question_number"];
+			setcookie("question_number", $question_number);
 			$answer_1 = $question_record["question_answer1"];
 			$answer_2 = $question_record["question_answer2"];
 			$answer_3 = $question_record["question_answer3"];
@@ -50,13 +53,21 @@
 	
 		# Ensures the cookie is set
 		$_COOKIE["topic_id"] = $topic_id;	
+		echo "<p>First time on the script</p>";
+		# Runs the question function with the value of 1 if it's the first time
+		question(1);
 	}
 	else
 	{
 		echo "<p>isset worked</p>";
+		# Runs the question function with updated number
+		$topic_id = $_COOKIE["topic_id"];
+		$question_number = $_COOKIE["question_number"];
+		question($question_number);
 	}
 
-	$topic_id = $_COOKIE["topic_id"];
+
+
 
 	
 
@@ -64,8 +75,6 @@
 	echo "<p> The topic ID from the cookie is: " . $_COOKIE["topic_id"] . "</p>";
 	echo "<p> The user ID from the cookie is: " . $_COOKIE["login"] . "</p>";
 	
-	
-	question();
 
 ?>
 
--- submit.php
+++ submit.php
@@ -5,7 +5,12 @@
 	# Takes the submitted data
 	$chosen_answer = $_POST["answers"];
 	$correct_answer = $_POST["correct"];
-
+	
+	# Increments the value in the question number cookie
+	$question_number = $_COOKIE["question_number"];
+	$question_number = $question_number + 1;
+	setcookie("question_number", $question_number);
+	
 	# Compares the chosen answer with the correct answer
 	if($chosen_answer == $correct_answer)
 	{
